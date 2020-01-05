326575756ddd1be659f104554efb89df55cd1551
Farzin <f_zaker@yahoo.com>
2020-01-05 16:38:59 -0500

    Externalize configuration

---
+++ config.groovy
@@ -0,0 +1,18 @@
+dataSource {
+    driverClassName = com.mysql.cj.jdbc.Driver
+    dialect = org.hibernate.dialect.MySQL8Dialect
+    dbCreate = 'update'
+    url = 'jdbc:mysql://localhost/f3'
+    username = 'root'
+    password = 'Salam123'
+    pooled = true
+    jmxExport = true
+}
+
+elasticsearch.url = 'https://vpc-fcs-mtsvc-cac1-dev-es-rqtpxcvnvahpgdxyktdr4hdkoq.ca-central-1.es.amazonaws.com'
+
+model {
+    path = 'd:/models/f3.model'
+    normalizer = 'd:/models/f3.normalizer'
+    dataFiles = 'd:/models/data/'
+}
--- grails-app/init/f3/Application.groovy
+++ grails-app/init/f3/Application.groovy
@@ -2,12 +2,29 @@ package f3
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-
+import grails.converters.JSON
 import groovy.transform.CompileStatic
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.Environment
+import org.springframework.core.env.MapPropertySource
 
-@CompileStatic
-class Application extends GrailsAutoConfiguration {
+//@CompileStatic
+class Application extends GrailsAutoConfiguration implements EnvironmentAware {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @Override
+    void setEnvironment(Environment env) {
+        def environment = env as ConfigurableEnvironment
+        def path = environment.getProperty('configuration').toString()
+        def file = new File(path)
+
+        if (file.exists()) {
+            ConfigObject config = new ConfigSlurper().parse(file.text)
+            def propertySource = new MapPropertySource(grails.util.Environment.getCurrent().name, config.toSorted())
+            environment.propertySources.addFirst(propertySource)
+        }
     }
 }
